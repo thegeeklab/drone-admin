c16e315630c988930ed7cf8c2368512bcf8f147e
Robert Kaussow <mail@thegeeklab.de>
2022-07-26 14:01:11 +0200

    feat: add reaper sub command to cleanup faulty agents (#4)

--- admin/util/util.go
+++ admin/util/util.go
@@ -1,5 +1,10 @@
 package util
 
+import (
+	"encoding/gob"
+	"os"
+)
+
 func Filter[T any](vs []T, f func(T) bool) []T {
 	filtered := make([]T, 0)
 	for _, v := range vs {
@@ -8,4 +13,24 @@ func Filter[T any](vs []T, f func(T) bool) []T {
 		}
 	}
 	return filtered
+}
+
+func WriteGob(filePath string, object interface{}) error {
+	file, err := os.Create(filePath)
+	if err == nil {
+		encoder := gob.NewEncoder(file)
+		err = encoder.Encode(object)
+	}
+	file.Close()
+	return err
+}
+
+func ReadGob(filePath string, object interface{}) error {
+	file, err := os.Open(filePath)
+	if err == nil {
+		decoder := gob.NewDecoder(file)
+		err = decoder.Decode(object)
+	}
+	file.Close()
+	return err
 }
